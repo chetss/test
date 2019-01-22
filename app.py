d3adc7d1b3c74cb2d7c523ccd5b16a3db07883cc
chetss <chetan1212sharma1212@gmail.com>
2019-01-22 21:04:17 +0530

    commit

--- app.py
+++ app.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-class shape:
-    
-    @property
-    def length(self):
-        return self.__length
-    
-    @length.setter
-    def length(self,length):
-        self.__length = length
-
-    @property
-    def width(self):
-        return self.__width
-    
-    @width.setter
-    def width(self,width):
-        self.__width = width
-
-    def display(self):
-        print(f'{self.length} {self.width}')
-
-a = shape()
-a.length = 10       
-a.width = 20
-a.display()
-=======
 class Kangaroo:
     """A Kangaroo is a marsupial."""
     
@@ -64,4 +37,3 @@ kanga.put_in_pouch(roo)
 
 print(kanga)
 print(roo)
->>>>>>> b920a3c3bdef387baa2d42771d9c1dddb69a43f2
