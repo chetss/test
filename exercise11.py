8927a9653236a1bc5d44bf4fc4f7f60d3db6f84e
chetss <chetan1212sharma1212@gmail.com>
2019-01-22 21:43:28 +0530

    commit

--- exercise11.py
+++
@@ -1,5 +0,0 @@
-def exercise1():
-    """Search the word in the word dict"""
-    with open('E:/python_with_virutal_env/my_project/words.txt') as file:
-        my_word_dict = {x: 1 for x in file.read().split()}
-        print(my_word_dict.get('zymase'))
