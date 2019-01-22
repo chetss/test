99601f210588ad3a23978bd5d8dc4fc213690628
chetss <chetan1212sharma1212@gmail.com>
2019-01-22 20:59:57 +0530

    commit

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print('inside app')
+class shape:
+    
+    @property
+    def length(self):
+        return self.__length
+    
+    @length.setter
+    def length(self,length):
+        self.__length = length
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self,width):
+        self.__width = width
+
+    def display(self):
+        print(f'{self.length} {self.width}')
+
+a = shape()
+a.length = 10       
+a.width = 20
+a.display()
